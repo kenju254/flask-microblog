6e9204fe7f8aa569c5c8a9368d962fd098719b79
Kenneth Kinyanjui <kenmbugua@gmail.com>
2016-08-17 21:30:49 +0300

    writing first view with jinja2 template

--- app/views.py
+++ app/views.py
@@ -1,16 +1,10 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname': 'Kenju'} #fake user
-    return'''
-    <html>
-        <head>
-            <title> Run With ''' + user['nickname'] + ''' </title>
-        </head>
-        <body>
-            <h1>Hello, ''' + user['nickname']  + '''</h1>
-        </body>
-    </html>
-'''
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
