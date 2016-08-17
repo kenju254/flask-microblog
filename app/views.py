e41870839e55da33182f0e3a6fed6c3fe2c03bea
Kenneth Kinyanjui <kenmbugua@gmail.com>
2016-08-17 18:23:59 +0300

    adding some html view with some variables

--- app/views.py
+++ app/views.py
@@ -3,4 +3,14 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World !"
+    user = {'nickname': 'Kenju'} #fake user
+    return'''
+    <html>
+        <head>
+            <title> Run With ''' + user['nickname'] + ''' </title>
+        </head>
+        <body>
+            <h1>Hello, ''' + user['nickname']  + '''</h1>
+        </body>
+    </html>
+'''
