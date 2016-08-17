9700fac586ae9f5e689baa5d74e24c6d984bdfb1
Kenneth Kinyanjui <kenmbugua@gmail.com>
2016-08-17 22:17:33 +0300

    Looping in the templates

--- app/views.py
+++ app/views.py
@@ -5,6 +5,17 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname': 'Kenju'} #fake user
+    posts = [
+        {
+            'author':{'nickname': 'Dennis'},
+            'body': 'This is one good day in San Francisco'
+        },
+        {
+            'author': {'nickname': 'Biggie'},
+            'body': 'I enjoyed watching Will Smith on Suicide Squad'
+        }
+    ]
     return render_template('index.html',
                            title='Home',
-                           user=user)
+                           user=user,
+                           posts=posts)
